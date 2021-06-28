d9bfa0ace8fd214d553b960c1f9b121e82a5d5ca
The-Slaith <34562469+The-Slaith@users.noreply.github.com>
2021-06-27 21:49:30 -0700

    Finished Controls Page
    I might change the sprites for the control key and the shift key. I'm also considering changing the fonts for everything to something similar to what I used for the keys.

--- Tetris Shape/Assets/Scripts/ControlsPanel.cs
+++ Tetris Shape/Assets/Scripts/ControlsPanel.cs
@@ -7,7 +7,7 @@ public class ControlsPanel : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.SetActive(false);
+        gameObject.SetActive(true);
     }
 
     // Update is called once per frame
