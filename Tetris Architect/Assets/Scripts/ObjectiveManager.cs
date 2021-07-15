34675885e5abb4bb2db34c4012dd7eba7b7d068b
The-Slaith <34562469+The-Slaith@users.noreply.github.com>
2021-07-14 22:52:45 -0700

    Finished Last Levels
    I'm going to add tips to all the levels soon and I'm probably going to add a tutorial to level 1, as well as add a "skip tutorial" button in the pause menu.

--- Tetris Architect/Assets/Scripts/ObjectiveManager.cs
+++ Tetris Architect/Assets/Scripts/ObjectiveManager.cs
@@ -190,7 +190,7 @@ public class ObjectiveManager : MonoBehaviour
     }
     void InitLevels() //I don't like how long these are.
     {
-        //level 1
+        //level 1, I might add a tutorial here just so the tutorial for level 6 is less surprising.
         levels.Add(new LevelData(1, 6, new int[,]{ { 3, 0 }, { 4, 0 }, { 5, 0 }, { 6, 0 },
                                                    { 3, 1 }, { 4, 1 }, { 5, 1 }, { 6, 1 },
                                                    { 3, 2 }, { 4, 2 }, { 5, 2 }, { 6, 2 },
@@ -209,9 +209,9 @@ public class ObjectiveManager : MonoBehaviour
         levels.Add(new LevelData(1, 10, new int[,] { { 4, 3 }, { 5, 3 }, { 5, 4 }, { 6, 4 }, { 6, 5 },
                                                      { 7, 5 }, { 7, 6 }, { 6, 6 }, { 6, 7 }, { 5, 7 },
                                                      { 5, 8 }, { 4, 8 }, { 4, 7 }, { 3, 7 }, { 3, 6 }, { 2, 6 }, { 2, 5 }, { 3, 5 }, { 3, 4 }, { 4, 4 } }, "The Ringer", 0.3f, 2));
-        //level 6
+        //level 6, an introduction to the "0 buffers" levels
         levels.Add(new LevelData(1, 0, new int[,] { { 4, 5 } }, "Just One Tile", 0.1f, 3));
-        //tutorial for level 6
+        //tutorial for level 6, I don't like that this is the first and only tutorial.
         tutorials.Add(new TutorialLevel(levels[5], new int[][,] { new int[,] { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 2, 0 }, { 9, 7 }, { 9, 8 }, { 8, 8 }, { 7, 8 } }, //J piece
                                                                   new int[,] { { 1, 1 }, { 2, 1 }, { 2, 2 }, { 3, 2 }, { 7, 5 }, { 8, 5 }, { 8, 6 }, { 9, 6 } }, //S piece
                                                                   new int[,] { { 1, 2 }, { 1, 3 }, { 2, 3 }, { 3, 3 }, { 6, 1 }, { 7, 1 }, { 8, 1 }, { 8, 2 }, { 2, 7 }, { 3, 7 }, { 4, 7 }, { 4, 8 } }, //L Piece
@@ -221,7 +221,7 @@ public class ObjectiveManager : MonoBehaviour
                                                                   new int[,] { { 3, 0 }, { 4, 0 }, { 3, 1 }, { 4, 1 } } }, new Piece[] { Piece.O, Piece.T, Piece.L }, 5, "The Magical Floating Orange." ));
         //level 7
         levels.Add(new LevelData(1, 0, new int[,] { { 4, 5 }, { 5, 5 }, { 4, 6 }, { 5, 6 }, { 4, 7 }, { 5, 7 } }, "Half and Whole", 0.05f, 5));
-        //level 8
+        //level 8, an introduction to the concept of buffers being seperate from the objective and the concept of "piercing" through a buffer to get to the other side.
         levels.Add(new LevelData(new int[,] { { 2, 3 }, { 2, 4 }, { 2, 5 }, { 2, 6 }, { 2, 7 }, { 2, 8 }, { 3, 8 }, { 4, 8 }, { 5, 8 }, { 6, 8 }, { 7, 8 }, { 7, 7 }, { 7, 6 }, { 7, 5 }, { 7, 4 }, { 7, 3 }, { 6, 3 }, { 5, 3 }, { 4, 3 }, { 3, 3 }, { 2, 3 } },
                                  0, new int[,] { { 4, 6 }, { 4, 5 }, { 5, 5 }, { 5, 6 } }, "The Vault", 0.2f, 3));
         //level 9
@@ -231,17 +231,17 @@ public class ObjectiveManager : MonoBehaviour
         levels.Add(new LevelData(new int[,] { { 0, 0 }, { 9, 0 }, { 1, 1 }, { 8, 1 }, { 2, 2 }, { 7, 2 }, { 3, 3 }, { 6, 3 }, { 4, 4 }, { 5, 4 },
                                               { 5, 5 }, { 4, 5 }, { 6, 6 }, { 3, 6 }, { 7, 7 }, { 2, 7 }, { 8, 8 }, { 1, 8 }, { 9, 9 }, { 0, 9 } }, 0,
                                  new int[,] { { 4, 18 }, { 5, 18 } }, "Double Cross", 0, 20));
-        //level 11
+        //level 11, an introduction to the concept of mutliple seperate objective regions. Was originally level 12 before the old level 11 was moved to the challenge levels.
         levels.Add(new LevelData(1, 0, new int[,] { { 2, 8 }, { 7, 8 } }, "The Abyss Stares Back", 0.1f, 4));
-        //level 12
+        //level 12, a combination of levels 8 and 11. Was originally 2 (very weirdly) intersecting diamonds, but top halves were cut off to allow for more room and make it easier. Was originally level 13 before the old level 11 was moved to the challenge levels.
         levels.Add(new LevelData(new int[,] { { 3, 7 }, { 6, 7 }, { 2, 8 }, { 4, 8 }, { 7, 8 }, { 5, 8 }, { 1, 9 }, { 5, 9 }, { 8, 9 }, { 4, 9 },
                                               { 0, 10 }, { 3, 10 }, { 6, 10 }, { 9, 10 } }, 0,
                                  new int[,] { { 1, 10 }, { 8, 10 } }, "A Poorly Drawn W (I Tried)", 0.1f, 4, tip: "It's actually 2 V's intersecting very weirdly. Oh you probably came here for a hint. The place where you go through the buffer will be very important, since it determines how you'll clear the line and how you'll build to the objective."));
-        //level 13
+        //level 13, ended looking even funnier than I imagined. Created as a replacement for the old level 11.
         levels.Add(new LevelData(new int[,] { { 4, 0 }, { 5, 0 }, { 3, 1 }, { 6, 1 }, { 2, 2 }, { 7, 2 }, { 1, 3 }, { 8, 3 }, { 0, 4 }, { 9, 4 },
                                               { 4, 9 }, { 5, 9 }, { 3, 8 }, { 6, 8 }, { 2, 7 }, { 7, 7 }, { 1, 6 }, { 8, 6 }, { 0, 5 }, { 9, 5 } }, 0,
                                  new int[,] { { 3, 4 }, { 3, 5 }, { 4, 6 }, { 5, 6 }, { 6, 5 }, { 6, 4 }, { 5, 3 }, { 4, 3 }, { 2, 10 }, { 7, 10 } }, "Open Wide!", 0.1f, 4));
-        //level 14
+        //level 14, an introduction to the "follow the path" levels.
         levels.Add(new LevelData(1, 5, new int[,] { { 3, 3 }, { 4, 3 }, { 5, 3 }, { 6, 3 }, { 7, 3 }, { 3, 4 }, { 4, 4 }, { 5, 4 }, { 6, 4 }, { 6, 5 }, { 6, 6 }, { 6, 7 }, { 6, 8 }, { 6, 9 },
                                                     { 7, 4 }, { 7, 5 }, { 7, 6 }, { 7, 7 }, { 7, 8 }, { 7, 9 }, { 5, 9 }, { 4, 9 }, { 3, 9 }, { 7, 10 }, { 6, 10 }, { 5, 10 }, { 4, 10 }, { 3, 10 } }, "Follow the Curve", 0.2f, 2, tip: "It's very hard to cleanly make a 2 tile wide line. Use buffers carefully."));
         //level 15
@@ -256,26 +256,30 @@ public class ObjectiveManager : MonoBehaviour
                                                     { 4, 5 }, { 5, 5 }, { 8, 5 }, { 9, 5 }, { 4, 6 }, { 5, 6 }, { 8, 6 }, { 9, 6 }, { 4, 7 }, { 5, 7 }, { 8, 7 }, { 9, 7 }, { 4, 8 }, { 5, 8 }, { 8, 8 }, { 9, 8 }, { 4, 9 }, { 5, 9 }, { 8, 9 }, { 9, 9 }, { 6, 8 }, { 7, 8 }, { 6, 9 }, { 7, 9 },
                                                     { 2, 6 }, { 3, 6 }, { 2, 7 }, { 3, 7 }, { 2, 8 }, { 3, 8 }, { 2, 9 }, { 3, 9 }, { 2, 10 }, { 3, 10 }, { 2, 11 }, { 3, 11 }, { 2, 12 }, { 3, 12 }, { 2, 13 }, { 3, 13 },
                                                     { 0, 14 }, { 1, 14 }, { 2, 14 }, { 3, 14 }, { 4, 14 }, { 5, 14 }, { 0, 15 }, { 1, 15 }, { 2, 15 }, { 3, 15 }, { 4, 15 }, { 5, 15 } }, "Thanks For Playing", 0.05f, 7));
-        //Challenge Level 1
+        //Challenge Level 1, based on levels 9, 10, 12, and 13 (an entire line is blocked off, requiring you to clear a line to get past it.)
         levels.Add(new LevelData(new int[,] { { 0, 7 }, { 1, 8 }, { 2, 7 }, { 3, 8 }, { 4, 7 }, { 5, 8 }, { 6, 7 }, { 7, 8 }, { 8, 7 }, { 9, 8 } }, 0, new int[,] { { 4, 10 }, { 4, 11 }, { 5, 10 }, { 5, 11 } }, "The Impenetrable Wall", 0.1f, 10, tip: "This is much harder than it initially seems. It may be easy to get to the first layer and place a piece to set up for the second layer, but clearing the line without making artifacts in either layer will be difficult. This time, you'll need to build up over the wall a bit and plan out how the lines will be cleared."));
-        //Challenge Level 2
+        //Challenge Level 2, based on levels 14-16 (you're forced to follow a "path")
         levels.Add(new LevelData(1, 2, new int[,] { { 1, 0 }, { 2, 0 }, { 3, 0 }, { 1, 1 }, { 2, 1 }, { 3, 1 }, { 1, 2 }, { 2, 2 }, { 3, 2 }, { 1, 3 }, { 2, 3 }, { 3, 3 }, { 1, 4 }, { 2, 4 }, { 3, 4 }, { 1, 5 }, { 2, 5 }, { 3, 5 }, { 1, 6 }, { 2, 6 }, { 3, 6 }, { 1, 7 }, { 2, 7 }, { 3, 7 }, { 0, 6 }, { 0, 7 },
                                                     { 4, 5 }, { 5, 5 }, { 4, 6 }, { 5, 6 }, { 4, 7 }, { 5, 7 }, { 3, 8 }, { 4, 8 }, { 5, 8 }, { 3, 9 }, { 4, 9 }, { 5, 9 }, { 3, 10 }, { 4, 10 }, { 5, 10 }, { 1, 10 }, { 1, 11 }, { 1, 12 }, { 2, 10 }, { 2, 11 }, { 2, 12 },
                                                     { 3, 11 }, { 3, 12 }, { 4, 11 }, { 4, 12 }, { 5, 11 }, { 5, 12 }, { 6, 10 }, { 6, 11 }, { 6, 12 }, { 7, 10 }, { 7, 11 }, { 7, 12 }, { 8, 10 }, { 8, 11 }, { 8, 12 }, { 9, 10 }, { 9, 11 }, { 9, 12 }, { 7, 13 }, { 8, 13 }, { 9, 13 }, { 7, 14 }, { 8, 14 }, { 9, 14 }, { 6, 15 }, { 7, 15 }, { 8, 15 }, { 9, 15 }, { 6, 16 }, { 7, 16 }, { 8, 16 }, { 9, 16 },
                                                     { 7, 9 }, { 8, 9 }, { 9, 9 }, { 7, 8 }, { 8, 8 }, { 9, 8 }, { 7, 7 }, { 8, 7 }, { 9, 7 }, { 7, 6 }, { 8, 6 }, { 9, 6 }, { 7, 5 }, { 8, 5 }, { 9, 5 }, { 7, 4 }, { 8, 4 }, { 9, 4 }, { 7, 3 }, { 8, 3 }, { 9, 3 }, { 7, 2 }, { 8, 2 }, { 9, 2 }, { 7, 1 }, { 8, 1 }, { 9, 1 }, { 5, 1 }, { 5, 2 }, { 5, 3 }, { 6, 1 }, { 6, 2 }, { 6, 3 } }, "The Snaking Maze", 0.1f, 5, tip: "Towering straight down is impossible in Tetris. Instead, try building across the gap at the bottom and clearing a line to get a foothold there. That way, you can use the buffers for something else. Also, when you get to the top, take advantage of the thin border by using a buffer to allow yourself to place junk tiles above the maze."));
-        //Challenge Level 3
-        levels.Add(new LevelData(new int[,] { { 0, 0 }, { 4, 0 }, { 5, 0 }, { 6, 0 }, { 7, 0 }, { 8, 0 }, { 9, 0 }, { 0, 1 }, { 4, 1 }, { 5, 1 }, { 6, 1 }, { 7, 1 }, { 8, 1 }, { 9, 1 }, { 0, 2 }, { 4, 2 }, { 5, 2 }, { 6, 2 }, { 7, 2 }, { 8, 2 }, { 9, 2 }, { 0, 3 }, { 4, 3 }, { 5, 3 }, { 9, 3 }, { 0, 4 }, { 4, 4 }, { 5, 4 }, { 9, 4 },
-                                              { 0, 5 }, { 1, 5 }, { 2, 5 }, { 3, 5 }, { 4, 5 }, { 5, 5 }, { 9, 5 }, { 0, 6 }, { 1, 6 }, { 2, 6 }, { 3, 6 }, { 4, 6 }, { 5, 6 }, { 9, 6 }, { 0, 7 }, { 1, 7 }, { 2, 7 }, { 3, 7 }, { 4, 7 }, { 5, 7 }, { 9, 7 }, { 0, 8 }, { 1, 8 }, { 5, 8 }, { 6, 8 }, { 9, 8 }, { 0, 9 }, { 1, 9 }, { 5, 9 }, { 6, 9 }, { 9, 9 },
-                                              { 0, 10 }, { 1, 10 }, { 5, 10 }, { 6, 10 }, { 7, 10 }, { 8, 10 }, { 9, 10 }, { 0, 11 }, { 1, 11 }, { 5, 11 }, { 6, 11 }, { 7, 11 }, { 8, 11 }, { 9, 11 }, { 0, 12 }, { 1, 12 }, { 5, 12 }, { 6, 12 }, { 7, 12 }, { 8, 12 }, { 0, 13 }, { 1, 13 }, { 2, 13 }, { 5, 13 }, { 6, 13 }, 
-                                              { 0, 14 }, { 1, 14 }, { 2, 14 }, { 3, 14 }, { 4, 14 }, { 5, 14 }, { 6, 14 }, { 2, 15 }, { 3, 15 }, { 4, 15 }, { 5, 15 }, { 6, 15 }, { 2, 16 }, { 3, 16 }, { 4, 16 }, { 5, 16 }, { 6, 16 }, { 2, 17 }, { 3, 17 }, { 4, 17 }, { 5, 17 }, { 6, 17 }, { 7, 17 }, { 8, 17 }, { 9, 17 } }, 2, new int[,] { { 0, 17 }, { 1, 17 } }, "Windows of Opportunity", 0.1f, 12, tip: "You'll need to bridge across the windows in a way that allows you to clear the lines once you've gotten there. I (light blue), J (blue), and L (orange) pieces can do well for most of the bridges, but for the last one you'll also need to use the S (green) piece to finish the bridge while still giving room for clearing the line. It's highly recommended you use 2-3 lines for each crossing."));
-        //Challenge Level 4
-        levels.Add(new LevelData(new int[,] { { 0, 0 }, { 1, 0 }, { 2, 0 }, { 7, 0 }, { 8, 0 }, { 9, 0 }, { 0, 1 }, { 1, 1 }, { 2, 1 }, { 7, 1 }, { 8, 1 }, { 9, 1 }, { 0, 2 }, { 1, 2 }, { 2, 2 }, { 7, 2 }, { 8, 2 }, { 9, 2 },
-                                              { 3, 3 }, { 6, 3 }, { 3, 4 }, { 4, 4 }, { 5, 4 }, { 6, 4 }, { 3, 5 }, { 4, 5 }, { 5, 5 }, { 6, 5 },
-                                              { 2, 6 }, { 7, 6 }, { 0, 7 }, { 1, 7 }, { 2, 7 }, { 7, 7 }, { 8, 7 }, { 9, 7 }, { 0, 8 }, { 1, 8 }, { 2, 8 }, { 7, 8 }, { 8, 8 }, { 9, 8 },
-                                              { 3, 9 }, { 6, 9 }, { 3, 10 }, { 4, 10 }, { 5, 10 }, { 6, 10 }, { 3, 11 }, { 4, 11 }, { 5, 11 }, { 6, 11 },
-                                              { 2, 12 }, { 7, 12 }, { 0, 13 }, { 1, 13 }, { 2, 13 }, { 7, 13 }, { 8, 13 }, { 9, 13 }, { 0, 14 }, { 1, 14 }, { 2, 14 }, { 7, 14 }, { 8, 14 }, { 9, 14 },
-                                              { 3, 15 }, { 6, 15 }, { 3, 16 }, { 4, 16 }, { 5, 16 }, { 6, 16 }, { 3, 17 }, { 4, 17 }, { 5, 17 }, { 6, 17 },
-                                              { 2, 18 }, { 7, 18 }, { 0, 19 }, { 1, 19 }, { 2, 19 }, { 7, 19 }, { 8, 19 }, { 9, 19 } }, 2, new int[,] { { 4, 18 }, { 5, 18 } }, "The Great Tower", 0.05f, 14, tip: "Remember when you had to clear a line to get past a barrier? This is just like that, only you have to do it 6 times with much less space. You don't have much space to place junk pieces, so if you feel RNG dealt you a bad hand, you can use a buffer to skip a section or clear lines to cycle through pieces."));
+        //Challenge Level 3, more tame version of old level 11.
+        levels.Add(new LevelData(new int[,] { { 4, 0 }, { 5, 0 }, { 6, 0 }, { 7, 0 }, { 8, 0 }, { 9, 0 }, { 4, 1 }, { 5, 1 }, { 6, 1 }, { 7, 1 }, { 8, 1 }, { 9, 1 }, { 4, 2 }, { 5, 2 }, { 6, 2 }, { 7, 2 }, { 8, 2 }, { 9, 2 }, { 4, 3 }, { 5, 3 }, { 4, 4 }, { 5, 4 },
+                                              { 0, 5 }, { 1, 5 }, { 2, 5 }, { 3, 5 }, { 4, 5 }, { 5, 5 }, { 0, 6 }, { 1, 6 }, { 2, 6 }, { 3, 6 }, { 4, 6 }, { 5, 6 }, { 0, 7 }, { 1, 7 }, { 2, 7 }, { 3, 7 }, { 4, 7 }, { 5, 7 }, { 0, 8 }, { 5, 8 }, { 6, 8 }, { 0, 9 }, { 5, 9 }, { 6, 9 },
+                                              { 0, 10 }, { 5, 10 }, { 6, 10 }, { 7, 10 }, { 8, 10 }, { 9, 10 }, { 0, 11 }, { 5, 11 }, { 6, 11 }, { 7, 11 }, { 8, 11 }, { 9, 11 }, { 0, 12 }, { 5, 12 }, { 6, 12 }, { 7, 12 }, { 8, 12 }, { 9, 12 }, { 0, 13 }, { 5, 13 }, { 6, 13 }, 
+                                              { 0, 14 }, { 1, 14 }, { 2, 14 }, { 3, 14 }, { 4, 14 }, { 5, 14 }, { 6, 14 }, { 0, 15 }, { 1, 15 }, { 2, 15 }, { 3, 15 }, { 4, 15 }, { 5, 15 }, { 6, 15 }, { 0, 16 }, { 1, 16 }, { 2, 16 }, { 3, 16 }, { 4, 16 }, { 5, 16 }, { 6, 16 }, { 0, 17 }, { 1, 17 }, { 2, 17 }, { 3, 17 }, { 4, 17 }, { 5, 17 }, { 6, 17 } }, 2, new int[,] { { 7, 17 }, { 8, 17 }, { 9, 17 } }, "Windows of Opportunity", 0.1f, 12, tip: "You'll need to bridge across the windows in a way that allows you to clear the lines once you've gotten there. I (light blue), J (blue), and L (orange) pieces can do well for most of the bridges, but for the last one you'll also need to use the S (green) piece to finish the bridge while still giving room for clearing the line. It's highly recommended you use 2-3 lines for each crossing."));
+        //Challenge Level 4, based on level 8 (the vault)
+        levels.Add(new LevelData(new int[,] { { 2, 7 }, { 2, 8 }, { 2, 9 }, { 2, 10 }, { 2, 11 }, { 3, 12 }, { 4, 12 }, { 5, 12 }, { 6, 12 }, { 7, 12 }, { 8, 11 }, { 8, 10 }, { 8, 9 }, { 8, 8 }, { 8, 7 }, { 7, 6 }, { 6, 6 }, { 5, 6 }, { 4, 6 }, { 3, 6 }, { 4, 9 }, { 5, 8 }, { 5, 10 }, { 6, 9 } }, 0, new int[,] { { 3, 7 }, { 3, 11 }, { 7, 7 }, { 7, 11 }, { 5, 9 } },
+                                 "Bullseye", 0.1f, 5, tip: "Here, you have to worry about getting to the center tile while making sure you get the bottom corner tiles (trust me, you can miss those if you focus too hard on the center)." /*don't ask*/ + " It's recommended to tower on both sides so that it's easier to clear lines, but you should do most of the work on the left side where you have more room."));
+        //Scrapped Level. Was originally going to be level 11 and was the entire reason that I made a challenge levels section, and yet was deemed too unfair to be a challenge level. 
+        //Scrapped due to lack of room causing too much RNG dependency. Could not be fixed no matter how much I increased the room.
+        //levels.Add(new LevelData(new int[,] { { 0, 0 }, { 1, 0 }, { 2, 0 }, { 7, 0 }, { 8, 0 }, { 9, 0 }, { 0, 1 }, { 1, 1 }, { 2, 1 }, { 7, 1 }, { 8, 1 }, { 9, 1 }, { 0, 2 }, { 1, 2 }, { 2, 2 }, { 7, 2 }, { 8, 2 }, { 9, 2 },
+        //                                      { 3, 3 }, { 6, 3 }, { 3, 4 }, { 4, 4 }, { 5, 4 }, { 6, 4 }, { 3, 5 }, { 4, 5 }, { 5, 5 }, { 6, 5 },
+        //                                      { 2, 6 }, { 7, 6 }, { 0, 7 }, { 1, 7 }, { 2, 7 }, { 7, 7 }, { 8, 7 }, { 9, 7 }, { 0, 8 }, { 1, 8 }, { 2, 8 }, { 7, 8 }, { 8, 8 }, { 9, 8 },
+        //                                      { 3, 9 }, { 6, 9 }, { 3, 10 }, { 4, 10 }, { 5, 10 }, { 6, 10 }, { 3, 11 }, { 4, 11 }, { 5, 11 }, { 6, 11 },
+        //                                      { 2, 12 }, { 7, 12 }, { 0, 13 }, { 1, 13 }, { 2, 13 }, { 7, 13 }, { 8, 13 }, { 9, 13 }, { 0, 14 }, { 1, 14 }, { 2, 14 }, { 7, 14 }, { 8, 14 }, { 9, 14 },
+        //                                      { 3, 15 }, { 6, 15 }, { 3, 16 }, { 4, 16 }, { 5, 16 }, { 6, 16 }, { 3, 17 }, { 4, 17 }, { 5, 17 }, { 6, 17 },
+        //                                      { 2, 18 }, { 7, 18 }, { 0, 19 }, { 1, 19 }, { 2, 19 }, { 7, 19 }, { 8, 19 }, { 9, 19 } }, 2, new int[,] { { 4, 18 }, { 5, 18 } }, "The Great Tower", 0.05f, 14, tip: "Remember when you had to clear a line to get past a barrier? This is just like that, only you have to do it 6 times with much less space. You don't have much space to place junk pieces, so if you feel RNG dealt you a bad hand, you can use a buffer to skip a section or clear lines to cycle through pieces."));
     }
 
     public void StartLevel(int level)
